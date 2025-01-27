86507bf8fa9165c12e0308570718798fd8328d19
tacgui <arata01998@gmail.com>
2025-01-27 13:45:54 -0500

    Agregamos la funcion agregar amigo, obtenemos el nombre del amigo sin espacios en blanco y validador de que obtenemos un nombre, agregamos una funcion de modificar elementos de una lista en HTML

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
+
+
+function modificarLista(elemento,texto){
+    document.querySelector(elemento,texto);
+}
+
+function agregarAmigo(){
+//Obtiene el nombre sin espacios
+    let nuevoAmigo=document.getElementById("amigo").value.trim();
+
+    if (nuevoAmigo == "" ) {
+        alert("Por favor, ingrese un nombre válido.");
+    }else{
+        amigos.push(nuevoAmigo);
+        console.log(amigos);
+    }
+}
