25b1213ed977485f3e4dd25f64ecab7f7eca93c7
tacgui <arata01998@gmail.com>
2025-03-06 20:33:27 -0500

    Modificacion final proyecto 1.0

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ let amigos=[];
 
 
 //
-function limpiar() {
+function borrarListaAmigos() {
     amigos=[];
     modificarElementoHTML("amigo","");
     modificarElementoHTML("listaAmigos","");
+    modificarElementoHTML("resultado","");
 }
 
 //Funcion que modifica un elemento id de HTML
@@ -19,10 +20,17 @@ function modificarElementoHTML(elemento,texto){
 function mostrarElementosLista(lista) {
     let mostrarElementos = "";
     for (let indice = 0; indice < lista.length; indice++) {
-        mostrarElementos += `<li>${lista[indice]}</li>`;        
+        mostrarElementos += `<li class="lista_amigos" onclick="eliminarAmigo(this)"><img src="${lista[indice][0]}" alt="Avatar de ${lista[indice][1]}" width="50" height="50">${lista[indice][1]}</li>`;        
     }
     return mostrarElementos;
 }
+
+
+function eliminarAmigo(elemento){
+    amigos=amigos.filter(amigos => amigos[1] !== elemento.textContent);
+    modificarElementoHTML("listaAmigos",mostrarElementosLista(amigos));
+}
+
 
 //Funcion que agrega a un amigo a la lista de amigos
 function agregarAmigo(){
@@ -33,7 +41,7 @@ function agregarAmigo(){
     if (nuevoAmigo == "" ) {
         alert("Por favor, ingrese un nombre válido.");
     }else{
-        amigos.push(nuevoAmigo);
+        amigos.push([generarAvatar(nuevoAmigo),nuevoAmigo]);
         modificarElementoHTML("listaAmigos",mostrarElementosLista(amigos));
     }
     //Limpiamos el casillero de texto
@@ -48,8 +56,12 @@ function numeroRandom(lista){
 
 //Funcion que sortea para obtener al amigo elegido.
 function sortearAmigo(){
-    let amigoElegido=amigos[numeroRandom(amigos)];
-    modificarElementoHTML("resultado",`El amigo secreto sorteado es: ${amigoElegido}`);
-    limpiar();
+    numeroAleatorio= numeroRandom(amigos);
+    modificarElementoHTML("resultado",`El amigo secreto sorteado es: <li class="resultado_amigo_elegido"><img src="${amigos[numeroAleatorio][0]}">${amigos[numeroAleatorio][1]}</li>`);
+    //limpiar();
     return ;      
 }
+
+function generarAvatar(nombreAmigo) {
+    return `https://api.dicebear.com/7.x/micah/svg?seed=${encodeURIComponent(nombreAmigo)}`;
+    }
